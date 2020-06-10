2c7469356b06b20bea5068e97bb395c9d7062dfe
Masiu <ciszekmarcell@gmail.com>
2020-06-10 14:14:00 +0200

    changes

--- src/routes/store.router.ts
+++
@@ -1,9 +0,0 @@
-/* eslint-disable import/no-unresolved */
-import { Router } from 'express'
-import { getAllStores } from '../controllers/store.controller'
-
-const router = Router()
-
-router.route('/').get(getAllStores)
-
-export { router }
--- src/utils/types.ts
+++
@@ -1,8 +0,0 @@
-/* eslint-disable prettier/prettier */
-import{ Request } from 'express'
-import{ IUser } from '../models/Documents'
-
-export interface IAuthRequest extends Request {
-  user: IUser;
-  token: string;
-}
